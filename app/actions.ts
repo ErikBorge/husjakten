ced9d0bc4429d3e84133bc0eb984b3dcfc2fe32b
ErikBorge <erikaasborge@gmail.com>
2024-09-20 10:13:17 +0200

    new mongo client startegy

--- app/actions.ts
+++ app/actions.ts
@@ -1,18 +1,54 @@
 "use server";
 
-// import { MongoClient } from "mongodb";
+// // import { MongoClient } from "mongodb";
+// import { NextResponse } from "next/server";
+// import client from "@/lib/mongodb";
+
+// export async function getAllHouses() {
+//   try {
+//     console.log("running client.connect()");
+
+//     const mongoClient = await client.connect();
+//     console.log("connected to client");
+//     console.log("choosing database");
+
+//     const database = mongoClient.db(process.env.MONGODB_DATABASE); // Replace with your database name
+//     console.log("choosing collection");
+//     const collection = database.collection(
+//       process.env.MONGODB_COLLECTION_ALL_HOUSES as string
+//     );
+//     console.log("finding data");
+//     const data = await collection.find({}).toArray();
+//     console.log("data found! returning now");
+
+//     return NextResponse.json(data);
+//   } catch (error: unknown) {
+//     if (error instanceof Error) {
+//       return NextResponse.json(
+//         { error: `Failed to fetch data: ${error.message}` },
+//         { status: 500 }
+//       );
+//     }
+//     return NextResponse.json(
+//       { error: "Failed to fetch data: Unknown error" },
+//       { status: 500 }
+//     );
+//   } finally {
+//     await client.close();
+//   }
+// }
 import { NextResponse } from "next/server";
-import client from "@/lib/mongodb";
+import clientPromise from "@/lib/mongodb";
 
 export async function getAllHouses() {
   try {
-    console.log("running client.connect()");
+    console.log("await clientPromise");
+    const client = await clientPromise;
 
-    const mongoClient = await client.connect();
-    console.log("connected to client");
+    console.log("clientPromise done");
     console.log("choosing database");
 
-    const database = mongoClient.db(process.env.MONGODB_DATABASE); // Replace with your database name
+    const database = client.db(process.env.MONGODB_DATABASE); // Replace with your database name
     console.log("choosing collection");
     const collection = database.collection(
       process.env.MONGODB_COLLECTION_ALL_HOUSES as string
@@ -33,7 +69,5 @@ export async function getAllHouses() {
       { error: "Failed to fetch data: Unknown error" },
       { status: 500 }
     );
-  } finally {
-    await client.close();
   }
 }
--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,31 +1,68 @@
+// import { MongoClient } from "mongodb";
+
+// if (!process.env.MONGODB_URI) {
+//   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+// }
+
+// const uri = process.env.MONGODB_URI;
+// const options = { appName: "devrel.template.nextjs" };
+
+// let client: MongoClient;
+
+// if (process.env.NODE_ENV === "development") {
+//   // In development mode, use a global variable so that the value
+//   // is preserved across module reloads caused by HMR (Hot Module Replacement).
+//   const globalWithMongo = global as typeof globalThis & {
+//     _mongoClient?: MongoClient;
+//   };
+
+//   if (!globalWithMongo._mongoClient) {
+//     globalWithMongo._mongoClient = new MongoClient(uri, options);
+//   }
+//   client = globalWithMongo._mongoClient;
+// } else {
+//   // In production mode, it's best to not use a global variable.
+//   client = new MongoClient(uri, options);
+// }
+
+// // Export a module-scoped MongoClient. By doing this in a
+// // separate module, the client can be shared across functions.
+
+// export default client;
+
 import { MongoClient } from "mongodb";
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
-}
+const uri = process.env.MONGODB_URI as string; // your mongodb connection string
+const options = {};
 
-const uri = process.env.MONGODB_URI;
-const options = { appName: "devrel.template.nextjs" };
+// declare global {
+//   let _mongoClientPromise: Promise<MongoClient>;
+// }
 
-let client: MongoClient;
-
-if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  const globalWithMongo = global as typeof globalThis & {
-    _mongoClient?: MongoClient;
-  };
-
-  if (!globalWithMongo._mongoClient) {
-    globalWithMongo._mongoClient = new MongoClient(uri, options);
+class Singleton {
+  private static _instance: Singleton;
+  private client: MongoClient;
+  private clientPromise: Promise<MongoClient>;
+  private constructor() {
+    this.client = new MongoClient(uri, options);
+    this.clientPromise = this.client.connect();
+    if (process.env.NODE_ENV === "development") {
+      // In development mode, use a global variable so that the value
+      // is preserved across module reloads caused by HMR (Hot Module Replacement).
+      // eslint-disable-next-line
+      (global as any).mongoClientPromise = this.clientPromise;
+    }
   }
-  client = globalWithMongo._mongoClient;
-} else {
-  // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options);
+
+  public static get instance() {
+    if (!this._instance) {
+      this._instance = new Singleton();
+    }
+    return this._instance.clientPromise;
+  }
 }
+const clientPromise = Singleton.instance;
 
-// Export a module-scoped MongoClient. By doing this in a
+// Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
-
-export default client;
+export default clientPromise;
