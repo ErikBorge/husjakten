e69562f6dcb2453e8e238316761b668702ac8775
ErikBorge <erikaasborge@gmail.com>
2024-09-20 10:15:06 +0200

    commented old get-all route

--- app/api/get-all/route.ts
+++ app/api/get-all/route.ts
@@ -1,43 +1,43 @@
 // import { MongoClient } from "mongodb";
-import { NextResponse } from "next/server";
-import client from "@/lib/mongodb";
+// import { NextResponse } from "next/server";
+// import client from "@/lib/mongodb";
 
-// const uri = process.env.MONGODB_URI as string;
-// const client = new MongoClient(uri);
+// // const uri = process.env.MONGODB_URI as string;
+// // const client = new MongoClient(uri);
 
-export async function GET() {
-  try {
-    console.log("running client.connect()");
+// export async function GET() {
+//   try {
+//     console.log("running client.connect()");
 
-    const mongoClient = await client.connect();
-    console.log("connected to client");
-    console.log("choosing database");
+//     const mongoClient = await client.connect();
+//     console.log("connected to client");
+//     console.log("choosing database");
 
-    const database = mongoClient.db(process.env.MONGODB_DATABASE); // Replace with your database name
-    console.log("choosing collection");
-    const collection = database.collection(
-      process.env.MONGODB_COLLECTION_ALL_HOUSES as string
-    );
-    console.log("finding data");
-    const data = await collection.find({}).toArray();
-    console.log("data found! returning now");
+//     const database = mongoClient.db(process.env.MONGODB_DATABASE); // Replace with your database name
+//     console.log("choosing collection");
+//     const collection = database.collection(
+//       process.env.MONGODB_COLLECTION_ALL_HOUSES as string
+//     );
+//     console.log("finding data");
+//     const data = await collection.find({}).toArray();
+//     console.log("data found! returning now");
 
-    return NextResponse.json(data);
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      return NextResponse.json(
-        { error: `Failed to fetch data: ${error.message}` },
-        { status: 500 }
-      );
-    }
-    return NextResponse.json(
-      { error: "Failed to fetch data: Unknown error" },
-      { status: 500 }
-    );
-  } finally {
-    await client.close();
-  }
-}
+//     return NextResponse.json(data);
+//   } catch (error: unknown) {
+//     if (error instanceof Error) {
+//       return NextResponse.json(
+//         { error: `Failed to fetch data: ${error.message}` },
+//         { status: 500 }
+//       );
+//     }
+//     return NextResponse.json(
+//       { error: "Failed to fetch data: Unknown error" },
+//       { status: 500 }
+//     );
+//   } finally {
+//     await client.close();
+//   }
+// }
 
 // from BE
 // export async function GET() {
